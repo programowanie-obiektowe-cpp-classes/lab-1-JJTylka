c9ce10b13dab8c3921c60cfb84278a0b0723c4a7
JJTylka <120095858+JJTylka@users.noreply.github.com>
2025-10-19 14:38:39 +0200

    Update Wektor2D.hpp
    Zaliczenie zejściówki nr.1

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,27 @@
+#pragma once
+
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+    double x;
+    double y;
+
+public:
+    Wektor2D() : x(0), y(0) {}
+    Wektor2D(double xVal, double yVal) : x(xVal), y(yVal) {}
+
+    void setX(double newX) { x = newX; }
+    void setY(double newY) { y = newY; }
+
+    double getX() const { return x; }
+    double getY() const { return y; }
+
+    Wektor2D operator+(const Wektor2D& other) const
+    {
+        return Wektor2D(x + other.x, y + other.y);
+    }
+
+    double operator*(const Wektor2D& other) const
+    {
+        return x * other.x + y * other.y;
+    }
 };
